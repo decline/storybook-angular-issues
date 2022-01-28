3faca0119cd669b7cb7702a74cadb2e93c09cac7
Dominik Huppmann <dominik.huppmann@textbroker.eu>
2022-01-28 16:26:41 +0100

    add more description

--- src/stories/array.component.ts
+++ src/stories/array.component.ts
@@ -4,7 +4,7 @@ import { Component, Input } from '@angular/core';
     selector: 'storybook-array',
     template: `
         <h1>6.4.0-alpha.34</h1>
-        <p class="error">Value is a string, but it should be an array!</p>
+        <p class="error">Value is a string, but it should be an array! Length of array is the length of the string.</p>
         <p>Array: {{ array | json }}</p>
         <p>Array length: {{ array.length }}</p>
     `
